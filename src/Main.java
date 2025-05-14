956f3afbbcd3eb6463ce63eb7a5f5333ab55d25b
taivs93 <taivs93@gmail.com>
2025-05-14 13:14:52 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import entity.Student;
 import manager.StudentManager;
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static final StudentManager studentManager = new StudentManager();
